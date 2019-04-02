93d18f27f7b13fe2c8a4f4a3c59b7a4e43057e26
Victor <victor.pettersson@wilsoncreative.se>
2019-04-02 07:41:13 +0200

    fixed register on login page

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,6 +1,30 @@
 <template>
   <form action="/">
-    <input type="email">
-    <input type="password">
+    <h1>{{ register ? 'Registrera dig' : 'Logga in' }}</h1>
+    <div>
+      <label for="email">Email</label>
+      <input type="email" name="email">
+    </div>
+    <div>
+      <label for="password">Lösenord</label>
+      <input type="password" name="password">
+    </div>
+    <small>Har du inte ett konto? Klicka <a @click="register = !register" href="#">här</a></small>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      register: false
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
+
