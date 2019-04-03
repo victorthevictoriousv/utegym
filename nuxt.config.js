d9d51a2deadde724ababf815a6e1ddd5b390c916
Victor <victor.pettersson@wilsoncreative.se>
2019-04-03 19:30:38 +0200

    added fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.typekit.net/ell8ymo.css' }
     ]
   },
 
