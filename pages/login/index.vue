857e9df2d6e71d3a88ff521063d7344a665ae943
Victor <victor.pettersson@wilsoncreative.se>
2019-04-04 07:42:18 +0200

    login styles

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="section__login section">
-    <h1 class="heading heading--lg">{{ register ? 'Registrera dig' : 'Logga in' }}</h1>
+    <h1 :class="'heading heading--lg ' + (register ? 'peach' : 'blue')">{{ register ? 'Registrera dig' : 'Logga in' }}</h1>
     <form class="login-form" action="/">
       <div class="login-form__input">
         <label for="email">Email</label>
@@ -10,8 +10,8 @@
         <label for="password">Lösenord</label>
         <input type="password" name="password">
       </div>
-      <small>Har du inte ett konto? Klicka <a @click="register = !register" href="#">här</a></small>
-      <button>{{ register ? 'Registrera mig' : 'Logga in' }}</button>
+      <small>{{ (register ? 'Har du redan ett konto? ' : 'Har du inte ett konto? ') + 'Klicka ' }}<a @click="register = !register" href="#">här</a></small>
+      <button :class="'login-form__button ' + (register ? 'peach--bg' : 'blue--bg')">{{ register ? 'Registrera mig' : 'Logga in' }}</button>
     </form>
   </section>
 </template>
@@ -28,17 +28,68 @@ export default {
 
 
 <style lang="scss" scoped>
-  .login-form {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 300px;
+@import '@/assets/scss/config.scss';
 
-    &__input {
-      display: flex;
-      justify-content: space-between;
-      width: 100%;
+.section__login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+
+  h1 {
+    margin-bottom: 40px;
+  }
+}
+
+.blue {
+  color: $blue;
+
+  &--bg {
+    background-color: $blue;
+  }
+}
+
+.peach {
+  color: $peach;
+
+  &--bg {
+    background-color: $peach;
+  }
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 90%;
+  max-width: 400px;
+
+  > :not(:last-child) {
+    margin-bottom: 20px;
+  }
+
+  &__input {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+
+    label {
+      width: 150px;
+      font-weight: 700;
+    }
+
+    input {
+      background-color: transparent;
+      color: $white;
+      font-size: inherit;
     }
   }
+
+  &__button {
+    padding: 10px 20px;
+  }
+}
 </style>
 
